1e63299a292fd613688dcbff53d6392eda8a29d0
anjumpaul <35442861+anjumpaul@users.noreply.github.com>
2020-04-30 10:28:43 +0530

    Redux configuration, Bottom-tab navigation (#9)

--- App.js
+++ App.js
@@ -1,34 +1,85 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
-import LoginScreen from 'ReactNativeStarterKit/src/screens/Login';
-import SignupScreen from 'ReactNativeStarterKit/src/screens/Signup';
-import ForgotPassword from 'ReactNativeStarterKit/src/screens/ForgotPassword';
+import Icon from 'react-native-vector-icons/Feather';
+import {Provider, connect} from 'react-redux';
+import {createStore} from 'redux';
 
-const App: () => React$Node = () => {
+import {
+  LoginScreen,
+  SignupScreen,
+  ForgotPassword,
+  Home,
+  Search,
+  Profile,
+} from 'ReactNativeStarterKit/src/screens';
+import reducer from 'ReactNativeStarterKit/src/reducer';
+
+const store = createStore(reducer);
+
+const Routers = ({isSignedIn}) => {
   const Stack = createStackNavigator();
+  const Tab = createBottomTabNavigator();
+
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen
-          name="Login"
-          component={LoginScreen}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen
-          name="Signup"
-          component={SignupScreen}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen
-          name="ForgotPassword"
-          component={ForgotPassword}
-          options={{headerShown: false}}
-        />
-      </Stack.Navigator>
+      {isSignedIn ? (
+        <Tab.Navigator
+          screenOptions={({route}) => ({
+            tabBarIcon: ({focused, color, size}) => {
+              let iconName;
+
+              if (route.name === 'Home') {
+                iconName = 'home';
+              } else if (route.name === 'Search') {
+                iconName = 'search';
+              } else {
+                iconName = 'user';
+              }
+              return <Icon name={iconName} size={size} color={color} />;
+            },
+          })}
+          tabBarOptions={{
+            activeTintColor: '#c2047f',
+            inactiveTintColor: 'gray',
+          }}>
+          <Tab.Screen name="Home" component={Home} />
+          <Tab.Screen name="Search" component={Search} />
+          <Tab.Screen name="Profile" component={Profile} />
+        </Tab.Navigator>
+      ) : (
+        <Stack.Navigator initialRouteName="Login">
+          <Stack.Screen
+            name="Login"
+            component={LoginScreen}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen
+            name="Signup"
+            component={SignupScreen}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen
+            name="ForgotPassword"
+            component={ForgotPassword}
+            options={{headerShown: false}}
+          />
+        </Stack.Navigator>
+      )}
     </NavigationContainer>
   );
 };
+
+const RouterContainer = connect((state) => ({isSignedIn: state.isSignedIn}))(
+  Routers,
+);
+
+const App = () => (
+  <Provider store={store}>
+    <RouterContainer />
+  </Provider>
+);
 
 export default App;
---
+++ src/screens/index.js
@@ -0,0 +1,9 @@
+import LoginScreen from 'ReactNativeStarterKit/src/screens/Login';
+import SignupScreen from 'ReactNativeStarterKit/src/screens/Signup';
+import ForgotPassword from 'ReactNativeStarterKit/src/screens/ForgotPassword';
+
+import Home from 'ReactNativeStarterKit/src/screens/Home';
+import Search from 'ReactNativeStarterKit/src/screens/Search';
+import Profile from 'ReactNativeStarterKit/src/screens/Profile';
+
+export {LoginScreen, SignupScreen, ForgotPassword, Home, Search, Profile};
